d478174d41c1c5eed30d3389703a452b93ce8444
GersonFeDutra <gersonfedutra@gmail.com>
2024-11-19 18:28:28 -0300

    templates 08 - variable templates

--- templates/main.cpp
+++ templates/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <type_traits>
+#include <vector>
 
 
 void write(const Vector<std::string> &vs)
@@ -17,6 +19,24 @@ void f2(Vector<std::string> &vs) // Vector of some strings
 }
 
 
+template <typename T> using space_vector = std::vector<T, 3>;
+
+template <class T> constexpr T viscosity = 0.4;
+template <class T> constexpr space_vector<T> external_acceleration = {T{}, T{-9.8}, T{}};
+auto vis2 = 2 * viscosity<double>;
+auto acc = external_acceleration<float>;
+
+
+template <typename T, typename T2>
+constexpr bool Assignable = std::is_assignable<T &, T2>::value; // is_assignable is a type trait
+
+template <typename T> void testing()
+{
+	static_assert(Assignable<T &, double>, "can't assign a double");
+	static_assert(Assignable<T &, string>, "can't assign a string");
+}
+
+
 int main(void)
 {
 	Vector<char> vc(200);           // vector of 200 characters
@@ -26,6 +46,6 @@ int main(void)
 	// init vectors
 
 	// use vectors
-	//write(vs);
-    f2(vs);
+	// write(vs);
+	f2(vs);
 }
